6f51cbd8b01bb7afd92200e02b084e2757915f78
blakelower <blakelower1999@gmail.com>
2020-04-22 18:01:28 -0700

    wrote tests

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,21 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe ('enhance.js', () => {
+    it('no errors', () => {
+        expect(true).toBeTruthy();
+    })
+    describe ('.enhancer()', function(){
+        it('return an item', function (){
+            expect(enhancer({
+                name: "skateboard", 
+                enhancement:10,
+                durability: 100
+            })).toEqual({
+                name: "skateboard",
+                enhancement: 10,
+                durability: 100 
+            })
+        })
+    })
+})
